375be533e4ca515d84dd3c0275d8105e5b40b886
Michael <mjpj@live.com>
2021-11-06 18:17:13 -0400

    commit tree 759e970d951c8f520be87f625fdb07a3b3d3980d

--- index.js
+++ index.js
@@ -1,34 +1,91 @@
-
-function distanceFromHqInBlocks(someValue) {
-  if (someValue > 42){
-    return someValue - 42
+function distanceFromHqInBlocks(block){
+  if (block > 42){
+    return block - 42;
   } else {
-    return 42 - someValue
+    return 42 - block;
+  }
+  }
+
+const distanceFromHqInFeet = (block) =>  distanceFromHqInBlocks(block) * 264;
+
+
+function distanceTravelledInFeet(start,stop){
+  if (start > stop){
+    return (start - stop) * 264;
+} else { 
+  return (stop - start)*264;
   }
 }
-let distanceFromHqInFeet = block => distanceFromHqInBlocks(block) * 264;
 
-function distanceTravelledInFeet(begin, end) {
-  if (begin < end){
-  return (end - begin) *264;
-  } else {
-  return (begin - end) *264
-  }
-}
 
-function calculatesFarePrice (one, two) {
-  const distance = distanceTravelledInFeet(one, two);
-
-  if (distance <= 400) {
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination)
+  
+  if (distance <= 400){
     return 0;
-  } else if (distance > 400 && distance <= 2000) {
-    return .02 * (distance - 400);
-  } else if (distance > 2000 && distance < 2500) {
+  } else if (distance > 400 && distance <= 2000) { 
+    return .02 *( distance - 400);
+  } else if (distance > 2000 && distance < 2500){
     return 25;
   } else {
-    return 'cannot travel that far';
+    return  'cannot travel that far';
   }
-}
+  }
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
